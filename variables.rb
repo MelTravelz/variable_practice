8dd41db41d02c5750254ddb70621d4f03fe59a4b
Melony Franchini <meltravelz@gmail.com>
2022-11-01 15:03:18 -0600

    Add put statements

--- variables.rb
+++ variables.rb
@@ -1,11 +1,24 @@
 day_of_week = "Tuesday"
+day_of_week # "Tuesday"
+
+day_of_week = "Wednesday"
+day_of_week # "Wednesday"
+
 workout_name = "squats"
 
 dumbbell_lbs = "15"
 sets = "3"
+sets # "3"
+
+sets = "5"
+sets # "5"
 
 miles_walked = "3.5"
 miles_swam = "1.25"
 
 completed_set = "true"
 workout_finished = "false"
+
+puts "What's today?"
+
+puts "What workout are you doing?"
