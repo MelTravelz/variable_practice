924dae741a84c3da50ef15c3b14e6077dfd6d4f5
Melony Franchini <meltravelz@gmail.com>
2022-11-03 18:35:04 -0600

    Fix issues and resubmit assignment

--- variables.rb
+++ variables.rb
@@ -1,24 +1,34 @@
+#Defining variables
 day_of_week = "Tuesday"
-day_of_week # "Tuesday"
-
-day_of_week = "Wednesday"
-day_of_week # "Wednesday"
-
 workout_name = "squats"
 
-dumbbell_lbs = "15"
-sets = "3"
-sets # "3"
+dumbbell_lbs = 15
+sets = 3
 
-sets = "5"
-sets # "5"
+miles_walked = 3.5
+miles_swam = 1.25
 
-miles_walked = "3.5"
-miles_swam = "1.25"
+completed_set = true
+workout_finished = false
 
-completed_set = "true"
-workout_finished = "false"
+#Reassigning Variables
 
+sets = 5
+
+workout_finished = false
+
+#Puts statements 
 puts "What's today?"
+puts day_of_week
 
 puts "What workout are you doing?"
+puts workout_name
+
+puts "How many sets did you do?"
+puts sets
+
+puts "How many miles did you swim today?"
+puts miles_swam
+
+puts "Did you finish your workout?"
+puts workout_finished
